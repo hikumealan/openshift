fa6e659cf9e2981b3e5a824ee74234f99c43ef3e
hikumealan <hikumealan@hotmail.com>
2014-01-01 16:47:29 -0500

    Added http response

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function(request, response){
-
+response.writeHead(200, {'Content-Type': 'text/plain'});
+response.write('Hello World');
+response.end();
 }).listen(80);
 console.log('HTTP Server has started.');
